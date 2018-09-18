59aed9ea10853216cab2022003ade49339cf6a7b
GabrielLinss <glins731@gmail.com>
2018-09-18 16:42:59 -0300

    add documentação com swagger2/swagger-ui

--- src/main/java/com/api/produtos/config/SecurityConfig.java
+++ src/main/java/com/api/produtos/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.api.produtos.config;
-
+/*
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -13,7 +17,18 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableWebSecurity
 @EnableAuthorizationServer
 @EnableResourceServer
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.csrf().disable().authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/swagger-ui.*").permitAll()
+		.antMatchers(HttpMethod.POST, "/swagger-ui.*").permitAll()
+		.antMatchers(HttpMethod.PUT, "/swagger-ui.*").permitAll()
+		.antMatchers(HttpMethod.DELETE, "/swagger-ui.*").permitAll()
+		.anyRequest().authenticated();
+	}
 	
 	@Bean
 	@Override
@@ -30,4 +45,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	public PasswordEncoder passwordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
 	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception{
+		web.ignoring().antMatchers("/swagger-ui.html");
+	}
 }
+*/
--- src/main/java/com/api/produtos/controller/ProdutoController.java
+++ src/main/java/com/api/produtos/controller/ProdutoController.java
@@ -18,6 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.api.produtos.model.ProdutoModel;
 import com.api.produtos.repository.ProdutoRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(value="API REST Produtos")
 @RestController
 @RequestMapping("/api/produto")
 public class ProdutoController {
@@ -25,6 +29,7 @@ public class ProdutoController {
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
+	@ApiOperation(value="Retorna uma lista de todos os produtos cadastrados")
 	@GetMapping(produces="application/json")
 	public @ResponseBody Iterable<ProdutoModel> listaProdutos() {
 		Iterable<ProdutoModel> listaProdutos = produtoRepository.findAll();
@@ -32,11 +37,13 @@ public class ProdutoController {
 		return listaProdutos;
 	}
 	
+	@ApiOperation(value="Salva um produto na base de dados")
 	@PostMapping
 	public ProdutoModel salvarProduto(@RequestBody @Valid ProdutoModel produto) {
 		return produtoRepository.save(produto);
 	}
 	
+	@ApiOperation(value="Deleta um produto pelo id")
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> excluir(@PathVariable Long id){
 		ProdutoModel produto = produtoRepository.getOne(id);
@@ -50,6 +57,7 @@ public class ProdutoController {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@ApiOperation(value="Busca um produto pelo id")
 	@GetMapping("/{id}")
 	public ResponseEntity<ProdutoModel> buscar(@PathVariable Long id){
 		ProdutoModel produto = produtoRepository.getOne(id);
@@ -61,6 +69,7 @@ public class ProdutoController {
 		return ResponseEntity.ok(produto);
 	}
 	
+	@ApiOperation(value="Atualiza um produto pelo id")
 	@PutMapping("/{id}")
 	public ResponseEntity<ProdutoModel> atualizar(@PathVariable Long id, @Valid @RequestBody ProdutoModel produto){
 		ProdutoModel produtoExistente = produtoRepository.getOne(id);
